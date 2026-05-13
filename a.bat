72d5e2c55604daa5039c9f8914770d1d7aac7984
Expellirmud <expellirmud@example.com>
2026-05-13 15:31:39 +0700

    Initial commit: Next.js Boilerplate setup for Citizen Service Center

---
+++ a.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Next.js Boilerplate setup for Citizen Service Center"
+git branch -M main
+git remote add origin https://github.com/expellirmud-dot/citizen-portal.git
+git push -u origin main
